a9d2defc162077095b98c6a042978fddfbc78a32
Ed Moore <ed@90seconds.tv>
2018-04-27 10:48:27 +1000

    Updating to latest typescript

--- test/calc.spec.ts
+++ test/calc.spec.ts
@@ -1,11 +1,9 @@
-/// <reference path="../typings/mocha/mocha.d.ts" />
-/// <reference path="../typings/chai/chai.d.ts"/>
-import Calculator from '../lib/calc';
-import { expect } from 'chai';
+import Calculator from '../lib/calc'
+import { expect } from 'chai'
 
 describe('Calculator', () => {
-    it('should add two numbers together', () => {
-        const calc : Calculator = new Calculator();
-        expect(calc.add(2, 3)).to.equal(5)
-    });
-});
+  it('should add two numbers together', () => {
+    const calc: Calculator = new Calculator()
+    expect(calc.add(2, 3)).to.equal(5)
+  })
+})
